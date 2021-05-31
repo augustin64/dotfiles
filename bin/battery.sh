fee5c41f02a478eb71e5349d7e1e1b882a165feb
augustin64 <augustin.lucas64@gmail.com>
2021-05-31 22:59:51 +0200

    Quick Fix & not displaying alert if power adaptator is connected

--- bin/battery.sh
+++ bin/battery.sh
@@ -5,7 +5,8 @@ export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
 battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+power_supply_state=$(cat /sys/class/power_supply/AC0/online)
 
-if [ $battery_level -le 10 ]; then
-    notify-send "Battery low" "Battery level is ${battery_level}% --urgency=critical"
+if [[ "$battery_level" -le 15 && "$power_supply_state" == 0 ]]; then
+    notify-send "Battery low" "Battery level is ${battery_level}%" --urgency=critical
 fi
