d600d7f9a54b2f158057c714f7946ada3ec19567
augustin64 <augustin.lucas64@gmail.com>
2022-02-22 14:46:55 +0100

    Update bin/battery.sh (power_supply support)

--- bin/battery.sh
+++ bin/battery.sh
@@ -7,7 +7,7 @@ export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
 battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)' | sort -r | head -n 1)
 
-power_supply_state=$(cat /sys/class/power_supply/AC/online);
+power_supply_state=$(cat /sys/class/power_supply/AC*/online);
 
 if [[ "$battery_level" -le 15 && "$power_supply_state" = 0 ]]; then
     notify-send "Battery low" "Battery level is ${battery_level}%" --urgency=critical;
