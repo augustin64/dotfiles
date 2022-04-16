21f172d0d78703f3f408bc7ad11cc573d0adb0b6
augustin64 <augustin.lucas64@gmail.com>
2022-04-16 15:29:52 +0200

    Weekly commit - 15 Apr 2022

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export XAUTHORITY=/home/augustin64/.Xauthority
+# $HOME may not be defined as the script is run from cron job
+HOME="/home/augustin64"
+
+export XAUTHORITY="$HOME/.Xauthority"
 
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
@@ -11,5 +14,5 @@ power_supply_state=$(cat /sys/class/power_supply/AC*/online);
 
 if [[ "$battery_level" -le 15 && "$power_supply_state" = 0 ]]; then
     notify-send "Battery low" "Battery level is ${battery_level}%" --urgency=critical;
-    mpg123 "/home/augustin64/Sounds/low_battery.mp3";
+    mpg123 "$HOME/Sounds/low_battery.mp3";
 fi
--- bin/power_supply_on.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export XAUTHORITY=/home/augustin64/.Xauthority
-export DISPLAY=:0
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-
-# /usr/bin/sudo -u augustin64 /usr/bin/mpg123 --server=/run/user/1000/pulse/native /home/augustin64/Sounds/low_battery.mp3 > /dev/null 2>&1
-
-# /usr/bin/notify-send -c low "Battery Charging" "Your power supply is now connected"
